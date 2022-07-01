079fbc0cb2bc4fea9b24ed5eddc6c26e0b766873
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-07-01 11:18:29 +0600

    add update api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require ('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -19,7 +19,7 @@ async function run(){
     await client.connect();
     const billCollection = client.db('power-hack').collection('bill');
 
-    
+    //post api
     app.post('/bill', async (req, res) => {
         const bill = req.body;
         // console.log(purchase)
@@ -28,12 +28,42 @@ async function run(){
         // console.log(result);
     });
 
+    //get api
+
     app.get('/bill', async(req, res) =>{
         const query ={};
         const cursur = billCollection.find(query);
         const bill = await cursur.toArray();
         res.send(bill);
     })
+
+    //delete
+     app.delete('/bill/:id',async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)}
+        const result = await billCollection.deleteOne(query);
+        res.send(result);
+     })
+     //update
+     app.put('/bill/:id', async(req, res) =>{
+        const id = req.params.id;
+        // console.log(id);
+        const update = req.body;
+        const filter = {_id: ObjectId(id)};
+        const options = {upsert: true};
+        const updateDoc = {
+            $set: {
+                name: update.name,
+                email: update.email,
+                phone: update.phone,
+                amount: update.amount,
+                
+            }
+        };     
+        const result = await billCollection.updateOne(filter, updateDoc, options);
+        res.send(result);
+
+    });
 
    } 
    finally{
